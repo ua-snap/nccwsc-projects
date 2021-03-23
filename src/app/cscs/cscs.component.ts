f78a3931ed0e5555ac73b7537475d32d4e2ab92b
Bob Torgerson <rltorgerson@alaska.edu>
2021-03-23 12:53:48 -0800

    Removed CASC from Midwest CASC identifier.

--- src/app/cscs/cscs.component.ts
+++ src/app/cscs/cscs.component.ts
@@ -9,7 +9,7 @@ export class CscsComponent implements OnInit {
 
   csc_ids = [
     {'id': 'alaska', 'name' : 'Alaska'},
-    {'id': 'midwest', 'name': 'Midwest CASC'},
+    {'id': 'midwest', 'name': 'Midwest'},
     {'id': 'northwest', 'name' : 'Northwest'},
     {'id': 'north-central', 'name' : 'North Central'},
     {'id': 'northeast', 'name' : 'Northeast'},
