950b4f994b2bddfe8d7329d608e90d6aa607bee8
Bob Torgerson <rltorgerson@alaska.edu>
2023-04-19 09:57:50 -0800

    Fixes CASC map resizing issues (#147)
    * Fixes CASC map for resizing with browser window and rebuilding the 2D canvas that shows the yellow area outlines on resize.
    
    * Possible fix for when minimizing text in browser window for CASC map.

--- src/app/cscs/cscs.component.ts
+++ src/app/cscs/cscs.component.ts
@@ -1,69 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, HostListener } from "@angular/core";
 import { UrlService } from "../url.service";
+import * as $ from "jquery";
+import "../../assets/jquery.maphilight.js";
 
 declare function imageMapResize(): void;
 
 @Component({
-  selector: 'app-cscs',
-  templateUrl: './cscs.component.html',
-  styleUrls: ['./cscs.component.scss']
+  selector: "app-cscs",
+  templateUrl: "./cscs.component.html",
+  styleUrls: ["./cscs.component.scss"],
 })
-
 export class CscsComponent implements OnInit {
-
-  config = {
-    fade: true,
-    alwaysOn: false,
-    neverOn: false,
-
-    // fill
-    fill: true,
-    fillColor: '#ffffff',
-    fillOpacity: 0.4,
-
-    // stroke
-    stroke: true,
-    strokeColor: '#f0ff00',
-    strokeOpacity: 1,
-    strokeWidth: 5,
-
-    // shadow:
-    shadow: false,
-    shadowColor: '#000000',
-    shadowOpacity: 0.8,
-    shadowRadius: 10
-  }
-
   csc_ids = [
-    {'id': 'alaska', 'name' : 'Alaska'},
-    {'id': 'midwest', 'name': 'Midwest'},
-    {'id': 'northwest', 'name' : 'Northwest'},
-    {'id': 'north-central', 'name' : 'North Central'},
-    {'id': 'northeast', 'name' : 'Northeast'},
-    {'id': 'pacific-islands', 'name' : 'Pacific Islands'},
-    {'id': 'southwest', 'name' : 'Southwest'},
-    {'id': 'south-central', 'name' : 'South Central'},
-    {'id': 'southeast', 'name' : 'Southeast'},
-    {'id': 'national-casc', 'name' : 'National CASC'},
-  ]
+    { id: "alaska", name: "Alaska" },
+    { id: "midwest", name: "Midwest" },
+    { id: "northwest", name: "Northwest" },
+    { id: "north-central", name: "North Central" },
+    { id: "northeast", name: "Northeast" },
+    { id: "pacific-islands", name: "Pacific Islands" },
+    { id: "southwest", name: "Southwest" },
+    { id: "south-central", name: "South Central" },
+    { id: "southeast", name: "Southeast" },
+    { id: "national-casc", name: "National CASC" },
+  ];
 
   topic_ids = [
-    {'id': 'drought-fire-extremes', 'name': 'Drought, Fire and Extreme Weather'},
-    {'id': 'landscapes', 'name': 'Landscapes'},
-    {'id': 'indigenous-peoples', 'name': 'Indigenous Peoples'},
-    {'id': 'science-tools', 'name': 'Science Tools for Managers'},
-    {'id': 'water-coasts-ice', 'name': 'Water, Coasts and Ice'},
-    {'id': 'wildlife-plants', 'name': 'Wildlife and Plants'},
-  ]
+    { id: "drought-fire-extremes", name: "Drought, Fire and Extreme Weather" },
+    { id: "landscapes", name: "Landscapes" },
+    { id: "indigenous-peoples", name: "Indigenous Peoples" },
+    { id: "science-tools", name: "Science Tools for Managers" },
+    { id: "water-coasts-ice", name: "Water, Coasts and Ice" },
+    { id: "wildlife-plants", name: "Wildlife and Plants" },
+  ];
 
-  constructor(private urlService: UrlService) { }
+  constructor(private urlService: UrlService, private el: ElementRef) {}
 
   ngOnInit() {
     this.urlService.setPreviousTitle(null);
+    this.imageResized();
   }
 
   imageResized() {
-    imageMapResize(); // Javascript function in imageMapResizer.min.js 
+    imageMapResize(); // Javascript function in imageMapResizer.min.js
+    $(this.el.nativeElement).find("img[usemap]").maphilight({
+      fade: false,
+      alwaysOn: false,
+      neverOn: false,
+
+      // fill
+      fill: true,
+      fillColor: "ffffff",
+      fillOpacity: 0.4,
+
+      // stroke
+      stroke: true,
+      strokeColor: "f0ff00",
+      strokeOpacity: 1,
+      strokeWidth: 5,
+    });
   }
 
+  @HostListener("window:resize", ["$event"])
+  onResize(event) {
+    this.imageResized();
+  }
 }
