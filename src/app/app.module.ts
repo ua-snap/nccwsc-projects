51a390faa8d19f0b13ff86eea5c0f078fba3be0b
Bob Torgerson <rltorgerson@alaska.edu>
2024-07-15 13:56:32 -0800

    Updated to Angular 18 wiht Leaflet modules installed.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from "@angular/common/http";
 import { HashLocationStrategy, LocationStrategy } from "@angular/common";
 
 import { AppComponent } from "./app.component";
@@ -11,7 +14,7 @@ import {
   NgbActiveModal,
   NgbDropdownModule,
 } from "@ng-bootstrap/ng-bootstrap";
-// import { LeafletModule } from "@bluehalo/ngx-leaflet";
+import { LeafletModule } from "@asymmetrik/ngx-leaflet";
 import { NgSelectModule } from "@ng-select/ng-select";
 import { GoogleAnalyticsService } from "./google-analytics.service";
 import { UmamiService } from "./umami.service";
@@ -38,39 +41,46 @@ import { MatCheckboxModule } from "@angular/material/checkbox";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { BreadcrumbComponent } from "./breadcrumb/breadcrumb.component";
 
-@NgModule({ declarations: [
-        AppComponent,
-        HeaderComponent,
-        FooterComponent,
-        ProjectComponent,
-        ProjectsComponent,
-        SearchComponent,
-        CscsComponent,
-        TopicsComponent,
-        MapComponent,
-        SearchNavComponent,
-        CscComponent,
-        ProjectResourceComponent,
-        BreadcrumbComponent,
-    ],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        FormsModule,
-        routing,
-        NgSelectModule,
-        NgbDropdownModule,
-        NgbModule,
-        FontAwesomeModule,
-        MatTableModule,
-        MatSortModule,
-        MatCheckboxModule,
-        BrowserAnimationsModule], providers: [
-        GoogleAnalyticsService,
-        UmamiService,
-        LocalJsonService,
-        SciencebaseService,
-        SearchService,
-        UrlService,
-        { provide: LocationStrategy, useClass: HashLocationStrategy },
-        provideHttpClient(withInterceptorsFromDi()),
-    ] })
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProjectComponent,
+    ProjectsComponent,
+    SearchComponent,
+    CscsComponent,
+    TopicsComponent,
+    MapComponent,
+    SearchNavComponent,
+    CscComponent,
+    ProjectResourceComponent,
+    BreadcrumbComponent,
+  ],
+  bootstrap: [AppComponent],
+  imports: [
+    LeafletModule,
+    BrowserModule,
+    FormsModule,
+    routing,
+    NgSelectModule,
+    NgbDropdownModule,
+    NgbModule,
+    FontAwesomeModule,
+    MatTableModule,
+    MatSortModule,
+    MatCheckboxModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [
+    GoogleAnalyticsService,
+    UmamiService,
+    LocalJsonService,
+    SciencebaseService,
+    SearchService,
+    UrlService,
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
+})
 export class AppModule {}
