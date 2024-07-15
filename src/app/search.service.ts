0011f11244a6fbe45f651fe9b4743aafd5425d2b
Bob Torgerson <rltorgerson@alaska.edu>
2024-07-15 15:38:54 -0800

    Updates to clear unused packages and upgrade those that remain to their highest allowable version.

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable, BehaviorSubject } from "rxjs";
 import { environment } from "../environments/environment";
-import "rxjs/Rx";
 
 @Injectable()
 export class SearchService {
