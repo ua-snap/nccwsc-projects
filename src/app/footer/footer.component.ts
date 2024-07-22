ad70594544265b530b244f5c435a8b027c052551
Bob Torgerson <rltorgerson@alaska.edu>
2024-07-22 11:55:20 -0800

    Remove OnInit and functionality from footer, removal of dead code.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,20 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Component, OnInit, Inject } from "@angular/core";
-import { environment } from "../../environments/environment";
-import { DOCUMENT } from "@angular/common";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-footer",
   templateUrl: "./footer.component.html",
   styleUrls: ["./footer.component.scss"],
 })
-export class FooterComponent implements OnInit {
-  currentVersion = "";
-  currentPage: string;
-  constructor(@Inject(DOCUMENT) private document: any) {
-    this.currentPage = this.document.location.href;
-  }
-  ngOnInit() {
-    this.currentVersion = environment.version;
-  }
-}
+export class FooterComponent {}
