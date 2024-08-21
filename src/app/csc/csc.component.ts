1f36069d88dec27727f910ff1db9c8f354f44f16
Bob Torgerson <rltorgerson@alaska.edu>
2024-08-21 10:06:02 -0800

    Adds a dropdown menu of CASCs or topics to switch between them (#211)
    * Working CASC and topic switching. Needs formatting.
    
    * Fixes loading issue with CASC page.
    
    * Change strategy for switching topics/cascs in header
    
    * Updates select for topic and CASCs to have a default indicating the user can pick a different topic or CASC.
    
    * Updates selects for topics and CASCs to show a value but for the selected option to swap to a default option upon the select being opened.
    
    * Changes text of default options.
    
    ---------
    
    Co-authored-by: Bruce Crevensten <bruce.crevensten@gmail.com>

--- src/app/csc/csc.component.ts
+++ src/app/csc/csc.component.ts
@@ -26,6 +26,7 @@ export class CscComponent implements OnInit {
   @ViewChild(MatSort) sort = new MatSort();
 
   faSearch = faSearch;
+  csc = null;
   sub: any;
   id: any;
   sbId: any;
@@ -41,9 +42,11 @@ export class CscComponent implements OnInit {
   title = null;
   dataLoading = true;
   searchTerm = "";
+  selectedCasc: any;
   csc_ids = {
     "5050cb0ee4b0be20bb30eac0": "National CASC",
     "4f831626e4b0e84f6086809b": "Alaska CASC",
+    "5e2f3f59e4b0a79317d422af": "Midwest CASC",
     "4f83509de4b0e84f60868124": "North Central CASC",
     "4f8c648de4b0546c0c397b43": "Northeast CASC",
     "4f8c64d2e4b0546c0c397b46": "Northwest CASC",
@@ -51,12 +54,12 @@ export class CscComponent implements OnInit {
     "4f8c652fe4b0546c0c397b4a": "South Central CASC",
     "4f8c6557e4b0546c0c397b4c": "Southeast CASC",
     "4f8c6580e4b0546c0c397b4e": "Southwest CASC",
-    "5e2f3f59e4b0a79317d422af": "Midwest CASC",
   };
 
   csc_english_ids = {
     "national-casc": "5050cb0ee4b0be20bb30eac0",
     alaska: "4f831626e4b0e84f6086809b",
+    midwest: "5e2f3f59e4b0a79317d422af",
     "north-central": "4f83509de4b0e84f60868124",
     northeast: "4f8c648de4b0546c0c397b43",
     northwest: "4f8c64d2e4b0546c0c397b46",
@@ -64,8 +67,23 @@ export class CscComponent implements OnInit {
     "south-central": "4f8c652fe4b0546c0c397b4a",
     southeast: "4f8c6557e4b0546c0c397b4c",
     southwest: "4f8c6580e4b0546c0c397b4e",
-    midwest: "5e2f3f59e4b0a79317d422af",
   };
+
+  csc_paths = {
+    "national-casc": "National CASC",
+    alaska: "Alaska CASC",
+    midwest: "Midwest CASC",
+    "north-central": "North Central CASC",
+    northeast: "Northeast CASC",
+    northwest: "Northwest CASC",
+    "pacific-islands": "Pacific Islands CASC",
+    "south-central": "South Central CASC",
+    southeast: "Southeast CASC",
+    southwest: "Southwest CASC",
+  };
+
+  csc_identifiers = Object.keys(this.csc_paths);
+  filtered_csc_identifiers = this.csc_identifiers;
 
   displayedColumns: string[] = [
     "fiscal_year",
@@ -85,6 +103,15 @@ export class CscComponent implements OnInit {
     private cdr: ChangeDetectorRef,
   ) {
     this.dataSource = new MatTableDataSource<any>();
+  }
+
+  onSelectClick() {
+    this.selectedCasc = "";
+  }
+
+  onCascChange(event: any) {
+    this.selectedCasc = event.target.value;
+    this.router.navigate(["/casc", this.selectedCasc]);
   }
 
   showAllProjects() {
@@ -241,6 +268,7 @@ export class CscComponent implements OnInit {
 
   ngOnInit() {
     this.sub = this.route.params.subscribe((params) => {
+      this.dataLoading = true;
       this.id = params["id"];
       if (params["topic"]) {
         this.current_topic = params["topic"].split("+");
@@ -251,93 +279,106 @@ export class CscComponent implements OnInit {
       if (params["status"]) {
         this.current_status = params["status"].split("+");
       }
-    });
-    if (this.id.length != 24) {
-      this.sbId = this.csc_english_ids[this.id];
-    } else {
-      this.sbId = this.id;
-    }
 
-    this.title = this.csc_ids[this.sbId];
-    this.urlService.setPreviousTitle(this.title);
-    this.urlService.setCurrentTitle(this.title);
-    this.localJson.loadCscProjects(this.sbId).subscribe((data) => {
-      this.cscProjectsList = data;
-      for (const project in this.cscProjectsList) {
-        for (const topic in this.cscProjectsList[project].topics) {
-          if (
-            this.topics.indexOf(this.cscProjectsList[project].topics[topic]) < 0
-          ) {
-            this.topics.push(this.cscProjectsList[project].topics[topic]);
-          }
-        }
-        if (
-          this.fiscal_years.indexOf(this.cscProjectsList[project].fiscal_year) <
-          0
-        ) {
-          this.fiscal_years.push(this.cscProjectsList[project].fiscal_year);
-        }
-        if (this.statuses.indexOf(this.cscProjectsList[project].status) < 0) {
-          this.statuses.push(this.cscProjectsList[project].status);
-        }
-
-        // Sort options
-        this.fiscal_years.sort().reverse();
-        this.topics.sort();
-        this.statuses.sort();
-        this.filteredCscProjectsList.push(this.cscProjectsList[project]);
-
-        // Principal investigators
-        if (
-          this.cscProjectsList[project].contacts.principal_investigators != null
-        ) {
-          this.cscProjectsList[project].investigators_formatted = "<ul>";
-          for (const pi of this.cscProjectsList[project].contacts
-            .principal_investigators) {
-            this.cscProjectsList[project].investigators_formatted +=
-              "<li>" + pi.name + "<i> (" + pi.organization + "</i>)</li>";
-          }
-          this.cscProjectsList[project].investigators_formatted += "</ul>";
-        } else {
-          this.cscProjectsList[project].investigators_formatted = "N/A";
-        }
-
-        // Topics
-        if (this.cscProjectsList[project].topics != null) {
-          this.cscProjectsList[project].topics_formatted = "<ul>";
-          for (const t of this.cscProjectsList[project].topics) {
-            this.cscProjectsList[project].topics_formatted +=
-              "<li>" + t + "</li>";
-          }
-          this.cscProjectsList[project].topics_formatted += "</ul>";
-        } else {
-          this.cscProjectsList[project].topics_formatted = "N/A";
-        }
-
-        // Status
-        if (!this.cscProjectsList[project].status) {
-          this.cscProjectsList[project].status = "N/A";
-        }
+      if (this.id.length != 24) {
+        this.sbId = this.csc_english_ids[this.id];
+      } else {
+        this.sbId = this.id;
       }
 
-      this.filterProjectsList();
-      this.dataLoading = false;
-      // Waits until the data is loaded to render the table
-      this.cdr.detectChanges();
-      // Applies the sorting to the table after it is available in the DOM
-      this.dataSource.sort = this.sort;
-      // Sets the default sorting to the fiscal year column to show the downward arrow
-      this.setInitialSort();
+      this.topics = [];
+      this.fiscal_years = [];
+      this.statuses = [];
 
-      this.dataSource.filterPredicate = (data, filter) => {
-        return (
-          data.fiscal_year.toString().includes(filter) ||
-          data.title.toLowerCase().includes(filter) ||
-          data.investigators_formatted.toLowerCase().includes(filter) ||
-          data.topics_formatted.toLowerCase().includes(filter) ||
-          data.status.toLowerCase().includes(filter)
+      this.title = this.csc_ids[this.sbId];
+      this.urlService.setPreviousTitle(this.title);
+      this.urlService.setCurrentTitle(this.title);
+      this.localJson.loadCscProjects(this.sbId).subscribe((data) => {
+        this.filtered_csc_identifiers = this.csc_identifiers.filter(
+          (key) => this.csc_paths[key] !== this.title,
         );
-      };
+        this.cscProjectsList = data;
+        for (const project in this.cscProjectsList) {
+          for (const topic in this.cscProjectsList[project].topics) {
+            if (
+              this.topics.indexOf(this.cscProjectsList[project].topics[topic]) <
+              0
+            ) {
+              this.topics.push(this.cscProjectsList[project].topics[topic]);
+            }
+          }
+          if (
+            this.fiscal_years.indexOf(
+              this.cscProjectsList[project].fiscal_year,
+            ) < 0
+          ) {
+            this.fiscal_years.push(this.cscProjectsList[project].fiscal_year);
+          }
+          if (this.statuses.indexOf(this.cscProjectsList[project].status) < 0) {
+            this.statuses.push(this.cscProjectsList[project].status);
+          }
+
+          // Sort options
+          this.fiscal_years.sort().reverse();
+          this.topics.sort();
+          this.statuses.sort();
+          this.filteredCscProjectsList.push(this.cscProjectsList[project]);
+
+          // Principal investigators
+          if (
+            this.cscProjectsList[project].contacts.principal_investigators !=
+            null
+          ) {
+            this.cscProjectsList[project].investigators_formatted = "<ul>";
+            for (const pi of this.cscProjectsList[project].contacts
+              .principal_investigators) {
+              this.cscProjectsList[project].investigators_formatted +=
+                "<li>" + pi.name + "<i> (" + pi.organization + "</i>)</li>";
+            }
+            this.cscProjectsList[project].investigators_formatted += "</ul>";
+          } else {
+            this.cscProjectsList[project].investigators_formatted = "N/A";
+          }
+
+          // Topics
+          if (this.cscProjectsList[project].topics != null) {
+            this.cscProjectsList[project].topics_formatted = "<ul>";
+            for (const t of this.cscProjectsList[project].topics) {
+              this.cscProjectsList[project].topics_formatted +=
+                "<li>" + t + "</li>";
+            }
+            this.cscProjectsList[project].topics_formatted += "</ul>";
+          } else {
+            this.cscProjectsList[project].topics_formatted = "N/A";
+          }
+
+          // Status
+          if (!this.cscProjectsList[project].status) {
+            this.cscProjectsList[project].status = "N/A";
+          }
+        }
+
+        this.filterProjectsList();
+        this.dataLoading = false;
+        // Waits until the data is loaded to render the table
+        this.cdr.detectChanges();
+        // Sets the selectedCasc to the default option
+        this.selectedCasc = "national-casc";
+        // Applies the sorting to the table after it is available in the DOM
+        this.dataSource.sort = this.sort;
+        // Sets the default sorting to the fiscal year column to show the downward arrow
+        this.setInitialSort();
+
+        this.dataSource.filterPredicate = (data, filter) => {
+          return (
+            data.fiscal_year.toString().includes(filter) ||
+            data.title.toLowerCase().includes(filter) ||
+            data.investigators_formatted.toLowerCase().includes(filter) ||
+            data.topics_formatted.toLowerCase().includes(filter) ||
+            data.status.toLowerCase().includes(filter)
+          );
+        };
+      });
     });
   }
   setInitialSort() {
