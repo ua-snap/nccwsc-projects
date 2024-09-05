c3e7295311dc41b104cb7b7332cdb505dbdf59bc
Bob Torgerson <rltorgerson@alaska.edu>
2024-09-04 16:24:36 -0800

    Possibly working configuration.

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  version: "4.3.1",
+  production: false,
+  baseURL: "http://cascprojects.org",
+  serviceURL:
+    "http://api.cascprojects.org.s3-website-us-west-2.amazonaws.com/casc-service",
+  sbmainURL: "https://www.sciencebase.gov",
+  projectsPath: "/projects/#",
+  urlPrefix: "/",
+};
